5ca5e2bc83679d72b6c09c3fb53b9045af0283db
Satyam Gautam <satyamgautam155@gmail.com>
2019-10-08 23:31:18 +0530

    walls

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 
 import React from 'react';
-import {Text, StyleSheet, View, ActivityIndicator, FlatList, Dimensions, Image, Animated, TouchableWithoutFeedback} from 'react-native';
+import {Text, StyleSheet, View, ActivityIndicator, FlatList, Dimensions, Image, Animated, TouchableWithoutFeedback, TouchableOpacity,CameraRoll} from 'react-native';
 import axios from 'axios';
-
+import { Ionicons } from '@expo/vector-icons';
+import {FileSystem} from 'expo';
+import { Permissions } from 'expo-permissions';
 const {height,width} = Dimensions.get('window')
 export default class App extends React.Component{
   constructor(){
@@ -35,6 +37,31 @@ export default class App extends React.Component{
   componentDidMount() {
     this.loadWallpapers()
   }
+  saveToCameraRoll = async (image) =>{
+    let cameraPermissions = await Permissions.getAsync
+    (Permissions.CAMERA_ROLL);
+    if (cameraPermissions.status !=='granted')
+    {
+     cameraPermissions = await Permissions.getAsync
+    (Permissions.CAMERA_ROLL);
+    }
+    if (cameraPermissions.status == 'granted')
+    {
+      FileSystem.downloadAsync(image.urls.regular,
+        FileSystem.documentDirectory+image.id+'.jpg').then(({uri}) =>{
+          CameraRoll.saveToCameraRoll(uri);
+          alert('saved')
+        }).catch(error => {
+          console.log(error);
+          
+        });
+
+
+    }
+    else{
+      alert('requires permission');
+    }
+  }
 
   showControls = (item) =>{
     this.setState((state)=>({
@@ -61,6 +88,11 @@ export default class App extends React.Component{
       transform:[{scale:this.state.scale}]
     }
 
+    const actionBarY = this.state.scale.interpolate({
+      inputRange:[0.9,1],
+      outputRange:[0,80]
+    });
+
     return(
       <View style ={{flex: 1}}>
       <View 
@@ -86,11 +118,36 @@ export default class App extends React.Component{
       />
       </Animated.View>
       </TouchableWithoutFeedback>
+      <Animated.View
+      style={[{
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height: 80,
+        backgroundColor: '#031740',
+        flexDirection:'row',
+        justifyContent:'space-around',
+      }, 
+      { transform: [{ translateY: actionBarY}] }]}>
+      
+      <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      <TouchableOpacity
+       activeOpacity={0.5}
+       onPress={() => this.loadWallpapers()}>
+
+      <Ionicons name="ios-refresh" color="#fff" size={40}/>
+    
+      </TouchableOpacity>
+      </View>
+      
+   
+      </Animated.View>
+    
       </View>
     )
   }
   render(){
-
 
     return this.state.isLoading ? (
       <View 
@@ -105,6 +162,7 @@ export default class App extends React.Component{
     ) : (
       <View style={{ flex: 1, backgroundColor: 'black'}}>
       <FlatList
+      scrollEnabled={!this.state.isImageFocused}
       horizontal
       pagingEnabled
       data={this.state.images}
